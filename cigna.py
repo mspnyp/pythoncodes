de9e3de1989be579fe967a3f9da554dae1b30741
mspnyp <59067893+mspnyp@users.noreply.github.com>
2020-05-28 17:04:46 -0400

    Update cigna.py
    Basically data is in list of tuples format

--- cigna.py
+++ cigna.py
@@ -1,4 +1,18 @@
+'''
+ Assume the days of current year is numbered 1 to 365.
 
+A Coverage is defined as range of dates with a coverage start and end/term date. 
+Eg: Cov(1, 30) means the person is covered for something for Jan of this year.
+
+Problem: Given a series of coverage data for a person, we need to find the longest continuous coverage. 
+The coverage may have overlap and/or gaps in coverage.
+ 
+Example data:
+
+class Cov(eff: Int, term: Int)
+
+val coverages = List(Cov(1, 20), Cov(21, 30), Cov(15, 25), Cov(28, 40), Cov(50, 60), Cov(61, 200)
+'''
 
 
 def continuosCoverage(coverages):
